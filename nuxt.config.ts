620690fea63a66b521554e3fb0f686dfd495e943
Josh Haines <Josh@JoshHaines.com>
2026-09-21 09:30:43 -0400

    feat: new favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,26 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: { lang: "en" },
-      link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+      ],
       meta: [
         {
           name: "theme-color",
